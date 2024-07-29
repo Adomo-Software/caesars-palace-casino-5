d0e3e55939767e807bcee9595f4bb22a70ba768b
Algirdas Alisauskas <ALALI@danskebank.lt>
2024-07-29 12:40:28 +0300

    update

---
+++ src/main/java/com/binaryin/DogHouse.java
@@ -0,0 +1,13 @@
+package com.binaryin;
+
+public class DogHouse {
+    private boolean dogInHouse;
+
+    public boolean isDogInHouse() {
+        return dogInHouse;
+    }
+
+    public void setDogInHouse(boolean dogInHouse) {
+        this.dogInHouse = dogInHouse;
+    }
+}
